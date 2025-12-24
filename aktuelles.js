0fadb30b2394933a622c2e66a823da44e65317d1
mariabrandenstein <Brandenstein@posteo.de>
2025-12-24 23:29:20 +0000

    Removed test change

--- aktuelles.js
+++ aktuelles.js
@@ -6,11 +6,5 @@ const newsData = [
         "titel": "Frohe Weihnachten & Ein gutes neues Jahr",
         "text": "Ich wünsche allen Klienten, Partnern und Besuchern meiner Website ein besinnliches Weihnachtsfest und einen kraftvollen Start in ein gesundes Jahr 2025. Vielen Dank für das Vertrauen in meine pädagogische Arbeit.",
         "bild": ""
-    },
-    {
-        "datum": "25.12.2025",
-        "titel": "Frohe Weihnachten & Ein gutes neues Jahr",
-        "text": "Ich wünsche allen Klienten, Partnern und Besuchern meiner Website ein besinnliches Weihnachtsfest und einen kraftvollen Start in ein gesundes Jahr 2025. Vielen Dank für das Vertrauen in meine pädagogische Arbeit.",
-        "bild": ""
     }
 ];
